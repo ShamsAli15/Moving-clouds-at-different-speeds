2367cc4c5f6a31b3fc8049f97beefef3d47dccc4
ShamsAli15 <91918772+ShamsAli15@users.noreply.github.com>
2021-10-07 13:35:20 +0200

    Update src.dart

--- Moving_clouds_at different_speeds/project/lib/src.dart
+++ Moving_clouds_at different_speeds/project/lib/src.dart
@@ -1,138 +1,138 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+
 class Challenges extends StatelessWidget {
-  const Challenges({ Key? key }) : super(key: key);
+  const Challenges({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Color(0xffa2c9ea),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                CarouselSlider(
+                  items: [
+                    Image(
+                      image: NetworkImage(
+                          'https://raw.githubusercontent.com/KareemShaban1/flutter_challenges/master/assets/challenge2/cloud.png'),
+                      height: 150.0,
+                      width: 150.0,
+                    ),
+                  ],
+                  options: CarouselOptions(
+                    height: 150.0,
+                    autoPlay: true,
+                    autoPlayInterval: Duration(milliseconds: 3000),
+                    autoPlayAnimationDuration: Duration(milliseconds: 3000),
+                    autoPlayCurve: Curves.linear,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                ),
+                CarouselSlider(
+                  items: [
+                    Image(
+                      image: NetworkImage(
+                          'https://raw.githubusercontent.com/KareemShaban1/flutter_challenges/master/assets/challenge2/cloud.png'),
+                      height: 150.0,
+                      width: 150.0,
+                    ),
+                  ],
+                  options: CarouselOptions(
+                    height: 150.0,
+                    // pauseAutoPlayOnManualNavigate: true,
+                    // pauseAutoPlayOnTouch: true,
+                    // pauseAutoPlayInFiniteScroll: true,
 
-      body: Stack
-      (children: [
-        SingleChildScrollView(
-          child: Column(children: [
-            CarouselSlider(
-              items: [Image(image: NetworkImage('https://raw.githubusercontent.com/KareemShaban1/flutter_challenges/master/assets/challenge2/cloud.png'),
-              height: 150.0,
-              width: 150.0,
-              ),
-              ], 
-              options: CarouselOptions(
-                height: 150.0,
-                autoPlay: true,
-                autoPlayInterval: Duration(milliseconds: 3000),
-                autoPlayAnimationDuration: Duration(milliseconds: 3000),
-                autoPlayCurve: Curves.linear,
-                scrollDirection: Axis.horizontal,
+                    // reverse: false,
 
+                    autoPlay: true,
+                    autoPlayInterval: Duration(milliseconds: 4000),
+                    autoPlayAnimationDuration: Duration(milliseconds: 4000),
+                    autoPlayCurve: Curves.linear,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                ),
+                CarouselSlider(
+                  items: [
+                    Image(
+                      image: NetworkImage(
+                          'https://raw.githubusercontent.com/KareemShaban1/flutter_challenges/master/assets/challenge2/cloud.png'),
+                      height: 150.0,
+                      width: 150.0,
+                    ),
+                  ],
+                  options: CarouselOptions(
+                    height: 150.0,
+                    // pauseAutoPlayOnManualNavigate: true,
+                    // pauseAutoPlayOnTouch: true,
+                    // pauseAutoPlayInFiniteScroll: true,
 
-              ),
+                    // reverse: false,
+
+                    autoPlay: true,
+                    autoPlayInterval: Duration(milliseconds: 2000),
+                    autoPlayAnimationDuration: Duration(milliseconds: 2000),
+                    autoPlayCurve: Curves.linear,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                ),
+                CarouselSlider(
+                  items: [
+                    Image(
+                      image: NetworkImage(
+                          'https://raw.githubusercontent.com/KareemShaban1/flutter_challenges/master/assets/challenge2/cloud.png'),
+                      height: 150.0,
+                      width: 150.0,
+                    ),
+                  ],
+                  options: CarouselOptions(
+                    height: 150.0,
+                    // pauseAutoPlayOnManualNavigate: true,
+                    // pauseAutoPlayOnTouch: true,
+                    // pauseAutoPlayInFiniteScroll: true,
+
+                    // reverse: false,
+
+                    autoPlay: true,
+                    autoPlayInterval: Duration(milliseconds: 3000),
+                    autoPlayAnimationDuration: Duration(milliseconds: 3000),
+                    autoPlayCurve: Curves.linear,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                ),
+                CarouselSlider(
+                  items: [
+                    Image(
+                      image: NetworkImage(
+                          'https://raw.githubusercontent.com/KareemShaban1/flutter_challenges/master/assets/challenge2/cloud.png'),
+                      height: 150.0,
+                      width: 150.0,
+                    ),
+                  ],
+                  options: CarouselOptions(
+                    height: 150.0,
+                    // pauseAutoPlayOnManualNavigate: true,
+                    // pauseAutoPlayOnTouch: true,
+                    // pauseAutoPlayInFiniteScroll: true,
+
+                    // reverse: false,
+
+                    autoPlay: true,
+                    autoPlayInterval: Duration(milliseconds: 2000),
+                    autoPlayAnimationDuration: Duration(milliseconds: 2000),
+                    autoPlayCurve: Curves.linear,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                ),
+              ],
             ),
-            
-            CarouselSlider(
-              items: [Image(image: NetworkImage('https://raw.githubusercontent.com/KareemShaban1/flutter_challenges/master/assets/challenge2/cloud.png'),
-              height: 150.0,
-              width: 150.0,
-              ),
-              ], 
-              options: CarouselOptions(
-                height: 150.0,
-                // pauseAutoPlayOnManualNavigate: true,
-                // pauseAutoPlayOnTouch: true,
-                // pauseAutoPlayInFiniteScroll: true,
-
-                // reverse: false,
-
-                autoPlay: true,
-                autoPlayInterval: Duration(milliseconds: 4000),
-                autoPlayAnimationDuration: Duration(milliseconds: 4000),
-                autoPlayCurve: Curves.linear,
-                scrollDirection: Axis.horizontal,
-
-
-              ),
-            ),
-            CarouselSlider(
-              items: [Image(image: NetworkImage('https://raw.githubusercontent.com/KareemShaban1/flutter_challenges/master/assets/challenge2/cloud.png'),
-              height: 150.0,
-              width: 150.0,
-              ),
-              ], 
-              options: CarouselOptions(
-                height: 150.0,
-                // pauseAutoPlayOnManualNavigate: true,
-                // pauseAutoPlayOnTouch: true,
-                // pauseAutoPlayInFiniteScroll: true,
-
-                // reverse: false,
-
-                autoPlay: true,
-                autoPlayInterval: Duration(milliseconds: 2000),
-                autoPlayAnimationDuration: Duration(milliseconds: 2000),
-                autoPlayCurve: Curves.linear,
-                scrollDirection: Axis.horizontal,
-
-
-              ),
-            ),
-            
-              CarouselSlider(
-              items: [Image(image: NetworkImage('https://raw.githubusercontent.com/KareemShaban1/flutter_challenges/master/assets/challenge2/cloud.png'),
-              height: 150.0,
-              width: 150.0,
-              ),
-              ], 
-              options: CarouselOptions(
-                height: 150.0,
-                // pauseAutoPlayOnManualNavigate: true,
-                // pauseAutoPlayOnTouch: true,
-                // pauseAutoPlayInFiniteScroll: true,
-
-                // reverse: false,
-
-                autoPlay: true,
-                autoPlayInterval: Duration(milliseconds: 3000),
-                autoPlayAnimationDuration: Duration(milliseconds: 3000),
-                autoPlayCurve: Curves.linear,
-                scrollDirection: Axis.horizontal,
-
-
-              ),
-            ),
-
-                        CarouselSlider(
-              items: [Image(image: NetworkImage('https://raw.githubusercontent.com/KareemShaban1/flutter_challenges/master/assets/challenge2/cloud.png'),
-              height: 150.0,
-              width: 150.0,
-              ),
-              ], 
-              options: CarouselOptions(
-                height: 150.0,
-                // pauseAutoPlayOnManualNavigate: true,
-                // pauseAutoPlayOnTouch: true,
-                // pauseAutoPlayInFiniteScroll: true,
-
-                // reverse: false,
-
-                autoPlay: true,
-                autoPlayInterval: Duration(milliseconds: 2000),
-                autoPlayAnimationDuration: Duration(milliseconds: 2000),
-                autoPlayCurve: Curves.linear,
-                scrollDirection: Axis.horizontal,
-
-
-              ),
-            ),
-
-           
-            
-          ],
           ),
-        ),
-         Center(
-              child: CircleAvatar(radius: 120.0,
+          Center(
+            child: CircleAvatar(
+              radius: 120.0,
               backgroundColor: Colors.white,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
@@ -140,54 +140,43 @@ class Challenges extends StatelessWidget {
                 children: [
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-
                     children: [
-                      Text('ATMOSPHERE',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w900,
-                        color: Colors.black
-
+                      Text(
+                        'ATMOSPHERE',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.black),
                       ),
+                      SizedBox(
+                        width: 15.0,
                       ),
-                      SizedBox(width: 15.0,),
-
-                      Text('SH',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w900,
-                        color: Colors.blue
-
+                      Text(
+                        'SH',
+                        style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.blue),
                       ),
+                      SizedBox(
+                        height: 15.0,
                       ),
-                      SizedBox(height: 15.0,),
-
-                      
-
                     ],
                   ),
-                  Text('SOFTWARE House',
-                      style: TextStyle(
+                  Text(
+                    'SOFTWARE House',
+                    style: TextStyle(
                         fontSize: 15.0,
                         fontWeight: FontWeight.w900,
-                        color: Colors.grey
-
-                      ),
-                      ),
-                
-              ],),
+                        color: Colors.grey),
+                  ),
+                ],
               ),
-            )
-      ],
+            ),
+          )
+        ],
       ),
-
-
-      
     );
-
-
-    
-
   }
 }
 
